90857fb2f87a54304a34af498045b7a3efd4b597
Rümeysa Esra Güngören <esra.gungoren6@gmail.com>
2026-04-10 16:58:10 +0300

    1_CSharp_Console

---
+++ 1_CSharp_Console/csharp_fundamentals.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string parents = "Mom and Dad ";
+
+            int myAge = 19;
+
+            double favoriteNumber = 4.64;
+
+            char firstLetter = 'E';
+
+            Console.WriteLine(parents + myAge + " " + favoriteNumber + " " + firstLetter);
+
+
+            Console.Write("Passenger Name is ");
+            string passengerName = Console.ReadLine();
+            Console.WriteLine("Welcome, " + passengerName + "!");
+
+            Console.Write("Enter your age: ");
+            int randomAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your age is " + randomAge);
+
+            Console.Write("Enter your password: ");
+            int password = int.Parse(Console.ReadLine());
+            if (password == 230208)
+            {
+                Console.WriteLine("Password is true.");
+            }
+            else
+            {
+                Console.WriteLine("Password is false.");
+            }
+
+            string[] name = new string[4];
+            int lngth = name.Length;
+
+            int[] numbers = { 4, 6, 12, 6 };
+            Array.Sort(numbers); 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+        }
+    }
+}
---
+++ 1_CSharp_Console/simple_bank_system.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double balance = 1000.50;
+            Console.Write("Enter your name: ");
+            string customerName = Console.ReadLine();
+            bool isRunning = true;
+
+            Console.WriteLine("********** WELCOME TO THE BANK SYSTEM **********");
+            Console.WriteLine("Customer: " + customerName);
+
+            while (isRunning)
+            {
+                Console.WriteLine("\n------- MENU -------");
+                Console.WriteLine("1. View Balance");
+                Console.WriteLine("2. Deposit Money");
+                Console.WriteLine("3. Withdraw Money");
+                Console.WriteLine("4. Exit");
+                Console.Write("Please select an option: ");
+
+                string input = Console.ReadLine();
+                int choice;
+
+                if (!int.TryParse(input, out choice))
+                {
+                    Console.WriteLine("Invalid input! Please enter a number (1-4).");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Current Balance: " + balance + "$");
+                        break;
+
+                    case 2:
+                        Console.Write("Enter the amount to deposit: ");
+                        double deposit;
+                        if (!double.TryParse(Console.ReadLine(), out deposit))
+                        {
+                            Console.WriteLine("Error: Please enter a numeric amount.");
+                            continue;
+                        }
+                        balance += deposit;
+                        Console.WriteLine("Success! New Balance: " + balance + "$");
+                        break;
+
+                    case 3:
+                        Console.Write("Enter the amount to withdraw: ");
+                        double withdraw;
+                        if (!double.TryParse(Console.ReadLine(), out withdraw))
+                        {
+                            Console.WriteLine("Error: Please enter a numeric amount.");
+                            continue;
+                        }
+
+                        if (withdraw > balance)
+                        {
+                            Console.WriteLine("Error: Insufficient funds!");
+                        }
+                        else
+                        {
+                            balance -= withdraw;
+                            Console.WriteLine("Success! New Balance: " + balance + "$");
+                        }
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Thank you for using our bank.");
+                        isRunning = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice! Please enter a number between 1 and 4.");
+                        break;
+                }
+            }
+        }
+    }
+}
